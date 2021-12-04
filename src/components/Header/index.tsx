e3434aed7fc197a2535a8be50aef754f2ff19921
Ingrid Larissa <larissaorcx@gmail.com>
2021-12-04 09:52:14 -0300

    alterando algumas configurações no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import style from './header.module.scss';
 export default function Header(): JSX.Element {
   return (
     <a className={style.Logo} href="/">
-      <Img src="/Logo.svg" alt="Logo" width={250} height={250} />
+      <Img src="/Logo.svg" alt="Logo" width={250} height={155} />
     </a>
   );
 }
